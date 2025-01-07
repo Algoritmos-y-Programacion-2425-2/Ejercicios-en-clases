75927ec6fb56e3d9753ecd36c60125985d1af182
aguerraUnimet <98197577+aguerraUnimet@users.noreply.github.com>
2025-01-07 08:31:09 -0400

    Welcome

---
+++ welcome/mini_calcu.py
@@ -0,0 +1,7 @@
+number = int(input("Please enter a number:"))
+number2 = int(input("Please enter a number 2:"))
+
+print("The result is +:", number+number2)
+print("The result is -:", number-number2)
+print("The result is *:", number*number2)
+print("The result is /:", number/number2)
