43c5488cf6d20a71abcc3f6609aaf45427347bd8
Laboratorio SL002 <sl002@unimet.local>
2025-01-24 08:31:57 -0400

    Estructuras combinadas

---
+++ Semana 3/Viernes/quiz.py
@@ -0,0 +1,122 @@
+products = {
+    "mobiles": {
+        "Apple": [
+            {
+                "id": 1,
+                "name": "iPhone 7",
+                "price": 300
+            },
+            {
+                "id": 2,
+                "name": "iPhone 8",
+                "price": 350
+            },
+            {
+                "id": 3,
+                "name": "iPhone Xr",
+                "price": 450
+            },
+            {
+                "id": 4,
+                "name": "iPhone Xs",
+                "price": 460
+            },
+            {
+                "id": 5,
+                "name": "iPhone 11",
+                "price": 900
+            },
+            {
+                "id": 6,
+                "name": "iPhone 12",
+                "price": 1100
+            },
+            {
+                "id": 7,
+                "name": "iPhone 13",
+                "price": 1300
+            },
+        ],
+        "Samsung": [
+            {
+                "id": 8,
+                "name": "Samsung Galaxy Beam",
+                "price": 150
+            },
+            {
+                "id": 9,
+                "name": "Samsung Galaxy S6",
+                "price": 200
+            },
+            {
+                "id": 10,
+                "name": "Samsung Galaxy S7",
+                "price": 300
+            },
+        ],
+        "Xiaomi": [
+            {
+                "id": 11,
+                "name": "Xiaomi Redmi Note 8",
+                "price": 250
+            },
+            {
+                "id": 12,
+                "name": "Xiaomi Redmi Note 8 Pro",
+                "price": 300
+            },
+        ]
+    },
+    "laptops": {
+        "DELL": [
+            {
+                "id": 13,
+                "name": "Dell Inspiron 15",
+                "price": 600
+            },
+            {
+                "id": 14,
+                "name": "Dell Latitude 14",
+                "price": 650
+            },
+        ],
+        "MAC": [
+            {
+                "id": 15,
+                "name": "MacBook Pro 13",
+                "price": 900
+            },
+            {
+                "id": 16,
+                "name": "MacBook M1",
+                "price": 1500
+            },
+        ]
+    },
+}
+
+while True:
+    option = input("Please enter a option: \n1- See inventory\n2- Purchse\n3- Exit\n->")
+    if option == "1":
+        product_option = input("Please enter a product option to see: \n1- Mobiles\n2- laptops\n3- Back\n->")
+        brands = None
+        if product_option == "1":
+            brands = products.get("mobiles")
+        elif product_option == "2":
+            brands = products.get("laptops")
+        else:
+            pass
+        if brands != None:
+            for brand_name, product_list in brands.items():
+                print(brand_name)
+                for product in product_list:
+                    print(f"id:{product.get("id")} - {product.get("name")}: ${product.get("price")} ")
+        else:
+            pass
+    elif option == "2":
+        pass
+    elif option == "3":
+        break
+    else:
+        print("Invalid option")
+        
